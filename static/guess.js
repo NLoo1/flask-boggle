23589932291466ded888dcdf87c456ff37c6a15c
Nicholas <NLoo1@outlook.com>
2024-01-10 17:52:39 -0500

    Added pycache to gitignore, added display message for input words

--- static/guess.js
+++ static/guess.js
@@ -1,18 +1,21 @@
 $guess = $('#btnSubmit')
-$correctGuess = $('.container .d-none')
+$correctGuess = $('#displayGuess')
 
 $guess.on('click', async function(e){
     e.preventDefault()
-    console.log("HELLO!")
+    // console.log("HELLO!")
     guessWord = document.querySelector('#inputGuess').value
     response = await axios.post("http://127.0.0.1:5000/check_word", {word: guessWord})
-    // console.log(response.data)
+    console.log(response.data)
+    $correctGuess.html("")
+    $correctGuess.removeClass("d-none")
     if(response.data['message'] == "WORKS!"){
+        $correctGuess.append(`${guessWord} works`)
     } 
     else if(response.data['message'] == "NOT A WORD"){
-
+        $correctGuess.append("<p>not a word</p>")
     }
     else{
-
+        $correctGuess.append("<p>Not on board</p>")
     }
 })
