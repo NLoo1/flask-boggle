9c4bd61d3bc719353cd4876bdfd28d86aa26b029
Nicholas L <NLoo1@outlook.com>
2024-01-10 16:22:17 -0500

    Refactored /check_word to check if word is valid from Boggle instance

--- static/guess.js
+++ static/guess.js
@@ -1,9 +1,18 @@
 $guess = $('#btnSubmit')
+$correctGuess = $('.container .d-none')
 
 $guess.on('click', async function(e){
     e.preventDefault()
     console.log("HELLO!")
     guessWord = document.querySelector('#inputGuess').value
     response = await axios.post("http://127.0.0.1:5000/check_word", {word: guessWord})
-    console.log(response.data)
+    // console.log(response.data)
+    if(response.data['message'] == "WORKS!"){
+    } 
+    else if(response.data['message'] == "NOT A WORD"){
+
+    }
+    else{
+
+    }
 })
